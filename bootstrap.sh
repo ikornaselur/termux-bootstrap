44c67f72aa87ab22cfcb8788e22dfa1213cb6d36
Axel Örn Sigurðsson <axel@absalon.is>
2020-09-04 10:45:49 +0200

    Move vim to the end

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@
 pkg up -y
 
 # Install packages
-pkg install -y neovim tmux nodejs python zsh git exa file mosh
+pkg install -y neovim tmux nodejs python zsh git exa file mosh neofetch
 
 #########
 # Shell #
@@ -12,24 +12,6 @@ pkg install -y neovim tmux nodejs python zsh git exa file mosh
 
 # Set up oh-my-zsh
 curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
-
-#######
-# Vim #
-#######
-
-# Clone dotfiles
-mkdir projects
-git clone https://github.com/ikornaselur/dotfiles projects/dotfiles
-
-# Set up neovim config
-mkdir -p .config/nvim
-ln -s ~/projects/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
-
-# Install vim-plug
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# Install neovim plugins
-nvim +PlugInstall +qall
 
 ##########
 # Python #
@@ -50,8 +32,30 @@ npm install -g yarn
 # Add aliases
 echo "alias vim=nvim" >> .zshrc
 echo "alias ls=exa" >> .zshrc
+echo "neofetch" >> .zshrc
+
+# Clear default motd
+rm /data/data/com.termux/files/usr/etc/motd
 
 # Set default shell
 chsh -s zsh
+
+#######
+# Vim #
+#######
+
+# Clone dotfiles
+mkdir projects
+git clone https://github.com/ikornaselur/dotfiles projects/dotfiles
+
+# Set up neovim config
+mkdir -p .config/nvim
+ln -s ~/projects/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+
+# Install vim-plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Install neovim plugins
+nvim +PlugInstall +qall
 
 echo "All done! Restart Termill..."
